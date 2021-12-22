9eca2a260d29f66235ed3c92d3486d0f3285e4db
Overlord <tanvir1005@student.sust.edu>
2021-12-22 22:56:59 +0600

    no commit

--- app/src/main/java/com/ahmed/drawingjava/Drawing.java
+++ app/src/main/java/com/ahmed/drawingjava/Drawing.java
@@ -1,6 +1,8 @@
 package com.ahmed.drawingjava;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -11,6 +13,9 @@ import androidx.annotation.Nullable;
 
 public class Drawing extends View {
     private Paint brush;
+
+    private Bitmap bitmap;
+
     public Drawing(Context context) {
         super(context);
         init();
@@ -28,13 +33,16 @@ public class Drawing extends View {
 //        super(context, attrs, defStyleAttr, defStyleRes);
 //    }
     public void init() {
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.tanvir);
         brush = new Paint(Paint.ANTI_ALIAS_FLAG);
         brush.setColor(Color.parseColor("green"));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(getMeasuredWidth()/2,getMeasuredHeight()/2,24f,brush);
+        canvas.drawBitmap(bitmap, (getMeasuredWidth()/2)-(bitmap.getWidth()/2)  , (getMeasuredHeight()/2)-(bitmap.getHeight() / 2), null);
+
+        // canvas.drawCircle(getMeasuredWidth()/2,getMeasuredHeight()/2,24f,brush);
 
         canvas.save();
         super.onDraw(canvas);
